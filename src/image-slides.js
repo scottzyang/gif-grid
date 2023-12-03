03fce1e9d7dcc2ec9431b95584cd4547067cbb10
scottzyang <scottzyang@gmail.com>
2023-12-03 16:24:37 -0600

    Set up initial components

---
+++ src/image-slides.js
@@ -0,0 +1,16 @@
+import { LitElement, css, html } from 'lit'
+
+export class ImageSlides extends LitElement {
+  constructor() {
+    super()
+    this.slides = 5
+    this.width = "300px"
+    this.height = "200px"
+  }
+
+  render() {
+    return html`
+      <
+    `
+  }
+}
